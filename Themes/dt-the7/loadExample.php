775db9a372b753301061b0c02c34034de130878d
Perron Jones <perronj@vmware.com>
2018-05-24 22:36:29 -0400

    query balances

--- Themes/dt-the7/loadExample.php
+++ Themes/dt-the7/loadExample.php
@@ -22,8 +22,7 @@ function initializeExample() {
  * Enqueue our JavaScriptssssss
  */
 function add_javascript() {
-	$page_title = $wp_query->post->post_title;
-	ChromePhp::log($page_title);
+	ChromePhp::log($_REQUEST);
 	// Enqueue our jQuery. You never know if an install is loading it!
 	wp_enqueue_script('jquery');
 	// Call our script that contains the Ajaxy goodness.
@@ -31,7 +30,8 @@ function add_javascript() {
 	// Although used for translation, this function allows us to load arbitrary JS built with PHP into the head of our WP theme
 	// Without modifying the themes header.php :) #magix
 	// We only need the admin ajax file in WordPress, so that's all we'll do.
-	wp_localize_script('ajax_getBalances', 'balancesAjax', array('ajaxurl' => admin_url('admin-ajax.php'), ));
+
+	wp_localize_script('ajax_getBalances', 'balancesAjax', array('ajaxurl' => admin_url('admin-ajax.php'), 'requestData' => $_REQUEST));
 }
 
 /**
@@ -47,30 +47,56 @@ function getBalances() {
 	// Check that we requested this from the input field and the nonce is valid. Prevents malicious users.
 	if ( ! isset( $_POST['submission'] ) && ! $_POST['submission'] && ! wp_verify_nonce( $_POST['nonce'], 'action_search_email_nonce' ) )
 		return;
-	
 
-	$identifier = "system=true&Filter1=Field12+Is_equal_to+" . $_POST['data'];
+
+	$identifier = "system=true&Filter1=Field200+Is_equal_to+" . $_POST['data'];
 	ChromePhp::log($identifier);
 	try{
+		//$forms = getForms(null);
+		//ChromePhp::log($forms);
 		$entries = getEntries($identifier);
 		//ChromePhp::log(print_r($entries));
 	} catch (Exception $e){
 		ChromePhp::log($e->getMessage());
 	}
-	
+
 	//echo json_encode(array('body' => wp_kses_post($_POST['data']) . "_posted", ));
-	echo json_encode(array('body' => array_values($entries)));;
+	echo json_encode(array('body' => array_values($entries)));
+	//echo json_encode(array('body' => array_values($forms)));
 	// This funciton is REQUIRED within WordPress or else you'll get 'parse' errors
 	// because there's a zero at the end of your JSON
 	wp_die();
 }
 
-$apiKey = 'k16c3f9c0jwz7dm';
+$formHashes = array(
+	'pmxpg7a0mnbsnb', //2018-camper-application-medical-form-deposit
+	's1h1wpu1e3p7m2', //2018-camper-application-medical-form-full-pay
+	'zx3659u01qss9c', //2018-middle-school-camp-application-deposit
+	'z5eqt7w169o76z' //2018-middle-school-camper-application-full-pay
+);
+$apiKey = '0QAK-VS36-I6BO-AEMY';
 $subdomain = 'wideopencamps';
-$domain = 'wufoo';
-function getEntries($identifier) {
-	$wrapper = new WufooApiWrapper('0QAK-VS36-I6BO-AEMY', 'wideopencamps');
-	return $wrapper->getEntries('k16c3f9c0jwz7dm', 'forms', "system=true&Filter1=Field12+Is_equal_to+" . $_POST['data']); 
+function getEntries($identifier = null) {
+	global $formHashes;
+
+	$entries = array();
+
+	foreach ($formHashes as $formHash) {
+		ChromePhp::log($formHash);
+		$entry = getEntry($formHash, $identifier);
+		ChromePhp::log($entry);
+		array_push($entries, $entry);
+	}
+
+	return $entries;
+}
+
+function getEntry($formHash, $identifier = null) {
+	global $apiKey;
+	global $subdomain;
+
+	$wrapper = new WufooApiWrapper($apiKey, $subdomain);
+	return $wrapper->getEntries($formHash, 'forms', $identifier);
 }
 
 /**
@@ -85,4 +111,5 @@ function add_button() {
 	// Adds our nonce and creates a unique key automatically! #moremagix
 	echo '</form>';
 }
+
 ?>
